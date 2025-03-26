9d7a772b39ee3f4aa16310f393d1a3eb30ada1a9
Lee Obungen <lobungen@github.com>
2025-03-26 17:56:47 -0500

    updated client

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -6,6 +6,8 @@ class AuthService {
   }
 
   loggedIn() {
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
     // TODO: return a value that indicates if the user is logged in
   }
   
@@ -14,15 +16,21 @@ class AuthService {
   }
 
   getToken(): string {
+    const loggedUser = localStorage.getItem('id_token') || '';
+    return loggedUser;
     // TODO: return the token
   }
 
   login(idToken: string) {
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
     // TODO: set the token to localStorage
     // TODO: redirect to the home page
   }
 
   logout() {
+    localStorage.removeItem('id_token');
+    window.location.assign('/');
     // TODO: remove the token from localStorage
     // TODO: redirect to the login page
   }
