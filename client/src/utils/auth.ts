84c19b3275590db96398fc34043fcce3e9173a1f
Lee Obungen <lobungen@github.com>
2025-04-03 17:14:12 -0500

    updated client/utils/auth.ts

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -7,32 +7,39 @@ class AuthService {
 
   loggedIn() {
     const token = this.getToken();
-    return !!token && !this.isTokenExpired(token);
-    // TODO: return a value that indicates if the user is logged in
+    // return !!token && !this.isTokenExpired(token);
+    return !!token;
   }
   
-  isTokenExpired(token: string) {
-    // TODO: return a value that indicates if the token is expired
+  // isTokenExpired(token: string) {
+  isTokenExpired(token: string): boolean {
+    try {
+      const decoded: JwtPayload = jwtDecode(token);
+      if (decoded.exp) {
+        return decoded.exp * 1000 < Date.now();
+      }
+      return true; // If no expiration, consider it expired
+    } catch (error) {
+      return true; // Invalid token is considered expired
+    }
   }
 
   getToken(): string {
     const loggedUser = localStorage.getItem('id_token') || '';
     return loggedUser;
-    // TODO: return the token
+
   }
 
   login(idToken: string) {
     localStorage.setItem('id_token', idToken);
     window.location.assign('/');
-    // TODO: set the token to localStorage
-    // TODO: redirect to the home page
+
   }
 
   logout() {
     localStorage.removeItem('id_token');
     window.location.assign('/');
-    // TODO: remove the token from localStorage
-    // TODO: redirect to the login page
+    
   }
 }
 
