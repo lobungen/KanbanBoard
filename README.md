a86edec668b1469dea32f26e68ab2f7cacb78860
Lee Obungen <lobungen@github.com>
2025-04-03 19:00:04 -0500

    connect to postgresql

---
+++ README.md
@@ -0,0 +1,14 @@
+# Kanban Board
+
+Module 14 Challenge
+
+The challenge this week is to add authentication with JWT to an existing Kanban board application.
+
+## Deployed Application
+
+Click here for [Deployed Application]()
+
+
+## Github Repo
+
+Click here for [Challenge 14 Repo](https://github.com/lobungen/KanbanBoard)
--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { ticketRouter } from './ticket-routes.js';
 import { userRouter } from './user-routes.js';
+import { authenticateToken } from '../../middleware/auth.js';
 
 const router = Router();
 
 router.use('/tickets', ticketRouter);
-router.use('/users', userRouter);
+router.use('/users', authenticateToken, userRouter);
 
 export default router;
